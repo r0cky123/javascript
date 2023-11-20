be8260a0af02f976428ff2c181b72d1b3d28c4d5
r0cky123 <151191972+r0cky123@users.noreply.github.com>
2023-11-20 12:37:34 +0000

    comparison

---
+++ basics/4_comparison.js
@@ -0,0 +1,20 @@
+//they all gives boolean value
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//problems qccur when diff.datatype
+// console.log("2">1); //true
+// console.log("02">1); //true
+//try not to use diff.datatype comparison
+
+//comparion with null does give unperdictable sometimes so avoid them
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//comparison convert null to a number,treating it as 0.//true
+
+// === strictly equal they also check values and datatype.
+console.log("2"==2); //true "2" convert to number
+console.log("2"===2);//false
