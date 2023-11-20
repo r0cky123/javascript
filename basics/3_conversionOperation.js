a094b54489972f487d8e17a68f5ea345a6103725
r0cky123 <151191972+r0cky123@users.noreply.github.com>
2023-11-20 12:18:51 +0000

    operations

--- basics/3_conversionOperation.js
+++ basics/3_conversionOperation.js
@@ -1,16 +1,16 @@
 //to convert in number
-let score="33"
-let age="33abc"
+// let score="33"
+// let age="33abc"
 
 //console.log(typeof score); //string
 //console.log(score); //33
 
 //let valueInNum=Number(score)  //Number() change the type of variable to number datatype
-let valueInNum=Number(age) 
-console.log(typeof valueInNum) //number
-console.log( valueInNum) //NaN=>not a number
+// let valueInNum=Number(age) 
+// console.log(typeof valueInNum) //number
+// console.log( valueInNum) NaN=>not a number
 //Number() changes the type to number but value of variable sometimes gives number or NaN.
-console.log(typeof NaN);
+// console.log(typeof NaN);
 //undefined coversion in number gives NaN
 //null conversion in number gives 0.
 //boolean value conversion in number gives true for 1 and false for 0.
@@ -20,4 +20,22 @@ console.log(typeof NaN);
 //if value is empty string then false and string then true
 
 //String()
+
+// **************Operation***************//
+
+//2**3=>2 pow of 3
+//% gives the remainder
+
+//we can add strings
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+//guideline in ecma 7.1.1
+
+let counter=100
+counter++
+console.log(counter);//101
+//prefix and postfix js mdn
 
