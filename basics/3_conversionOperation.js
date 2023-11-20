56ee6b939f63e41fc24a16f11c9ef23894f73b1a
r0cky123 <151191972+r0cky123@users.noreply.github.com>
2023-11-20 10:23:54 +0000

    conversion of datatype

---
+++ basics/3_conversionOperation.js
@@ -0,0 +1,23 @@
+//to convert in number
+let score="33"
+let age="33abc"
+
+//console.log(typeof score); //string
+//console.log(score); //33
+
+//let valueInNum=Number(score)  //Number() change the type of variable to number datatype
+let valueInNum=Number(age) 
+console.log(typeof valueInNum) //number
+console.log( valueInNum) //NaN=>not a number
+//Number() changes the type to number but value of variable sometimes gives number or NaN.
+console.log(typeof NaN);
+//undefined coversion in number gives NaN
+//null conversion in number gives 0.
+//boolean value conversion in number gives true for 1 and false for 0.
+//if only string then in coversion to number gives NaN.let name="ritwik"
+
+//Boolean()
+//if value is empty string then false and string then true
+
+//String()
+
