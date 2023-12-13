5f6f4234d3ec3c46f1724ad15dbfd840e224549b
r0cky123 <r2079814@gmail.com>
2023-12-13 07:38:43 +0000

    complete class

---
+++ class andoops/5.mathpi.js
@@ -0,0 +1,7 @@
+//pi ki value change karna hai
+//getOwnPropertyDescriptor is a object property
+//(Object.getOwnPropertyDescriptor(chai, "name")); chai me se name(property) nikal na hai
+// defineProperty
+//method nhi chahiye console me if use karo
+//https://stackoverflow.com/questions/8312459/iterate-through-object-properties
+//enumerable false means iterable nhi hoga
---
+++ class andoops/6.getand set.js
@@ -0,0 +1,4 @@
+//yadi get kiya hai toh set bhi karna hoga and vice-versa
+//this._password 
+//ways-- object,function,class
+//
